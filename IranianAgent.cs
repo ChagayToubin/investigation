0a32787ffa78e5a92f8f0a70e6587893c8fd00db
chagay <chagaytoubin@gmail.com>
2025-06-15 22:55:33 +0300

    finish first version

---
+++ IranianAgent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace investigationChagay
+{
+    public class IranianAgent
+    {
+        public List<Sensor> listSensor;
+        public string Name;
+        public int Rank;
+
+        public List<string> gessList;
+        public int countTrueGess = 0;
+        public IranianAgent(string name,int rank,List<Sensor>list)
+        {
+            Name = name;
+            Rank = rank;
+            listSensor = list;
+            gessList = CopySensorNamesToList();
+
+
+        }
+        public List<string> CopySensorNamesToList()
+        {
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < listSensor.Count; i++)
+            {
+                string name = listSensor[i].Name;
+                result.Add(name);
+            }
+
+            return result;
+        }
+
+    }
+}
